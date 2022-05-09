7b95984f6b76e1daec3e00fbb754c261171d2e27
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2022-05-09 14:57:50 -0500

    gastos funcionalidad acabada

--- gestionar-gastos/src/app/pages/admin/ver-gastos-familia/ver-gastos-familia.page.ts
+++ gestionar-gastos/src/app/pages/admin/ver-gastos-familia/ver-gastos-familia.page.ts
@@ -3,7 +3,7 @@ import { Gasto } from 'src/app/domain/gasto';
 import { GastosService } from 'src/app/services/gastos.service';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { take } from 'rxjs/operators';
-import { AlertController, MenuController } from '@ionic/angular';
+import { AlertController, MenuController,LoadingController } from '@ionic/angular';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -27,6 +27,7 @@ export class VerGastosFamiliaPage implements OnInit {
     private router: Router, 
     private gastoService: GastosService,
     private alertCtrl: AlertController,
+    private loadingController: LoadingController,
     private auth :  AuthenticationService,
     public menuCtrl: MenuController ) {
       this.menuCtrl.enable(false)
@@ -34,42 +35,48 @@ export class VerGastosFamiliaPage implements OnInit {
 
   async ngOnInit() {
     this.sessionUser = await this.auth.getUserAuth()
-    this.obtenerGastosFml()    
+    return await this.loadingController.create({ }).then(a => {
+      a.present().then(async () => {
+        try {
+          this.obtenerGastosFml()
+          a.dismiss().then(() => console.log('abort presenting'))          
+        } catch (error) {
+          console.log('ERROR al cargar datos')
+          this.alert = "Ocurrió un error al cargar sus datos"
+          this.advice = 'Por favor, inténtelo de nuevo'
+
+          this.genericAlert(this.alert, this.advice)
+
+        } finally {
+          a.dismiss().then(() => console.log('abort presenting'))
+        }
+
+      }) 
+    })
+        
   }  
   async obtenerGastosFml(){     
-    await this.sessionUser.pipe(take(1)).subscribe(async user =>{
+    this.sessionUser.pipe(take(1)).subscribe(async user =>{
       try {        
         this.usuario = await this.auth.getUsuario(user.email)
-        await this.usuario.pipe(take(1)).subscribe(async user =>{
-          console.log(user[0].uid)
-        })
-        
-        this.usuario.forEach((element) => {
-          console.log("1")
-          this.usuarios=this.gastoService.obtenerusrFamilia(element[0].id_familia)
-          this.usuarios.forEach((element) => {   
-            console.log("2")        
-            for (let index = 0; index < element.length; index++) {
-              console.log("3")
-              this.gastos=this.gastoService.obtenerGastos(element[index].uid)        
-              this.gastos.forEach((element) => {
-                  for (let index1 = 0; index1 < element.length; index1++) {
-                    console.log("4")
-                    let aux:Gasto=new Gasto()
-                    aux.id=element[index1].id        
-                    aux.monto=element[index1].monto
-                    aux.id_usuario=element[index1].id_usuario
-                    aux.id_categoria=element[index1].id_categoria
-                    aux.fecha=element[index1].fecha
-                    aux.descripcion=element[index1].descripcion                        
-                    this.gastosF.push(aux)        
-                  }          
-                });
+        this.usuario.pipe(take(1)).subscribe(async user =>{
+          this.usuarios=this.gastoService.obtenerusrFamilia(user[0].id_familia)          
+          this.usuarios.pipe().subscribe(async user =>{
+            this.gastos=this.gastoService.obtenerGastos(user[0].uid)
+            this.gastos.pipe(take(1)).subscribe(async gasto =>{
+              for (let index = 0; index < gasto.length; index++) {
+                let aux:Gasto=new Gasto()
+                aux.id=gasto[index].id        
+                aux.monto=gasto[index].monto
+                aux.id_usuario=gasto[index].id_usuario
+                aux.id_categoria=gasto[index].id_categoria
+                aux.fecha=gasto[index].fecha
+                aux.descripcion=gasto[index].descripcion                        
+                this.gastosF.push(aux)
               }
-            });
-        });        
-        
-        
+            })            
+          })         
+        })        
       } catch(error){
           console.log(error)  
           this.alert = "Ocurrió un error inesperado al cargar la informacion del usuario"
--- gestionar-gastos/src/app/pages/all/registrar-gasto/registrar-gasto.page.ts
+++ gestionar-gastos/src/app/pages/all/registrar-gasto/registrar-gasto.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Gasto } from 'src/app/domain/gasto';
 import { GastosService } from 'src/app/services/gastos.service';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { AlertController, MenuController,LoadingController } from '@ionic/angular';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
 
 @Component({
   selector: 'app-registrar-gasto',
@@ -10,16 +14,61 @@ import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 })
 export class RegistrarGastoPage implements OnInit {
   gasto:Gasto=new Gasto();
+  usuario:any  
+
+  alert: string
+  advice: string
+
+  private sessionUser : any
 
   constructor(private route: ActivatedRoute,
     private router: Router, 
-    private gastoService: GastosService) { }
+    private gastoService: GastosService,
+    private alertCtrl: AlertController,
+    private auth :  AuthenticationService,
+    private loadingController: LoadingController,
+    public menuCtrl: MenuController ) {
+      this.menuCtrl.enable(false)
+    }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.sessionUser = await this.auth.getUserAuth()    
   }
-  registrarGasto(){
-    this.gasto.id_usuario="123"
-    this.gasto.id_categoria="123"
-    this.gastoService.guardar(this.gasto)
+  async registrarGasto(){
+    return await this.loadingController.create({ }).then(a => {
+      a.present().then(async () => {
+        this.sessionUser.pipe(take(1)).subscribe(async user =>{
+          try {        
+            this.usuario = await this.auth.getUsuario(user.email)
+            this.usuario.pipe(take(1)).subscribe(async user =>{
+              this.gasto.id_usuario=user[0].uid
+              this.gastoService.guardar(this.gasto)       
+            })        
+          } catch(error){
+              console.log(error)  
+              this.alert = "Ocurrió un error inesperado al cargar la informacion del usuario"
+              this.advice = 'Por favor, inténtelo de nuevo'
+        
+              return this.genericAlert(this.alert, this.advice)
+          }finally {
+            a.dismiss().then(() => console.log('abort presenting'))
+          }
+        })
+
+      }) 
+    })
+  }
+  async genericAlert(alert_message, advice){
+
+    const prompt = await this.alertCtrl.create({  
+      header: 'Lo sentimos',  
+      subHeader: alert_message,
+      message: advice,  
+      
+      buttons: ['Aceptar']  
+    }); 
+
+    await prompt.present()
+
   }
 }
