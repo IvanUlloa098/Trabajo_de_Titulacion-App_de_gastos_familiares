377d89510845db3d4e759e68cb16f90d882e492a
IvanUlloa098 <iulloa@est.ups.edu.ec>
2022-06-24 13:06:22 -0500

    Correccion

--- gestionar-gastos/src/environments/environment.ts
+++ gestionar-gastos/src/environments/environment.ts
@@ -15,7 +15,7 @@ export const environment = {
   },
 
   RG_FUNC_PATH: "https://us-central1-gestionar-gastos.cloudfunctions.net/regressionReq",
-  googleWebClientId : "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"
+  googleWebClientId : "519785918958-jfm7103u07hu0sfoprgku3jvp7kk7ire.apps.googleusercontent.com"
 
 };
 
